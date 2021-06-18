e6cf51c8f1e20fdfd15dd5d87f3cf81023e3c3da
Zayne Panaino <Zayne.panaino@gmail.com>
2021-06-18 20:58:10 +0200

    enhanced the efficiency of the application by adding redux store, which allows the user to refresh the page without losing their cart items

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './Redux/store';
+import { store, persistor } from './Redux/store';
 
 import './index.css';
 import App from './App';
@@ -12,10 +13,12 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-    <App />
+      <PersistGate persistor={persistor}> 
+        <App />
+      </PersistGate> 
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
-
+ 
 reportWebVitals();
